e030c9b9bd6ebd370fa3df67a42a0cff31511eed
MineGamer69 <aaryankapoor2003@gmail.com>
2023-02-26 17:35:24 -0500

    added check for invalid choice

--- animalpicker.java
+++ animalpicker.java
@@ -22,8 +22,10 @@ public class animalpicker {
             System.out.println(" (_, o o ,_)");
             System.out.println("    /    \\");
             System.out.println("  `--|__|--'");
+        } else{
+            System.out.println("Invalid choice. Please retry!");
         }
-        System.out.println("Just to restore commit");
+        
     }
 }
 
