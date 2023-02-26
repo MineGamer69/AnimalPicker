a0342c0612f8048e05cbf4e60ff535989ff54c2b
MineGamer69 <aaryankapoor2003@gmail.com>
2023-02-26 17:45:26 -0500

    Added cat ascii values

--- animalpicker.java
+++ animalpicker.java
@@ -18,7 +18,15 @@ public class animalpicker {
                 System.out.println(" /\\_/\\");
                 System.out.println("( o.o )");
                 System.out.println(" > ^ <");
-            }  else{
+            }else if (animalChoice.equalsIgnoreCase("fish")) {
+                System.out.println("     ><> ");
+                System.out.println("  ,-'`~`'-.");
+                System.out.println(" (_, o o ,_)");
+                System.out.println("    /    \\");
+                System.out.println("  `--|__|--'");
+                validInput = true;
+            }
+            else{
                 System.out.println("Invalid choice. Please try again!");
             }
         } while(!validInput);
